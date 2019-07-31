ef5ea4abea5ccd97b9f16d49ead3a1dda224ae7b
dogspace <rflorintsev@gmail.com>
2019-07-31 15:05:24 -0500

    Finished Commands + Handler

--- src/Commands.lua
+++ src/Commands.lua
@@ -30,17 +30,41 @@ function parsePlayers(playerArg)
 			end
 		end
 	end
-	print("Detected no players")
+	--print("Detected no players") -This should not be the case.
 	return {}
 end
 
 function commands.kick(...)  --ALWAYS put lowercase
-    --TODO: Start transfer of commands
+	local args = {...}
+	local reason = "An admin has decided to kick you." --Change this to whatever. This signifies the default.
+	local players = parsePlayers(args[1])
+	if #args > 1 then
+		reason = args[2]
+	end
+	for i,player in pairs(players) do
+    	player:Kick(reason)
+	end
 end
 
+function commands.tempadmin(...)  --ALWAYS put lowercase
+	local args = {...}
+	local time = 300
+	local players = parsePlayers(args[1])
+	if #args > 1 then
+		time = args[2]
+	end
+	for i,player in pairs(players) do
+		coroutine.wrap(function()
+			script.parent.Admin_Handler:SendMessage("Admin", player)
+			wait(time)
+			script.parent.Admin_Handler:SendMessage("UnAdmin", player)
+		end)()
+	end
+end
 
 script.OnServerMessage:connect(
-    function(CallType, player)
-        --TODO: Put Handler.
+    function(command, args)
+		if #args < 1 then return end
+        commands[command](unpack(args))
     end
 )
