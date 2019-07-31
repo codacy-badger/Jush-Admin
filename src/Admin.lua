ef8edf8a36edca945fcdf1cd84a0c4a70aa87eb1
dogspace <rflorintsev@gmail.com>
2019-07-31 13:59:50 -0500

    Start basic command parsing

--- src/Admin.lua
+++ src/Admin.lua
@@ -1,15 +1,29 @@
--- Made by Jush
--- Jush Admin [Open Source]
--- If you want to contribute to this admin, submit a pull request at https://github.com/JushBJJ/Jush-Admin/pulls
--- If you have a problem, issue it at https://github.com/JushBJJ/Jush-Admin/issues
-
 local Owner_User=game:GetOwnerUsername()
 local Prefix=";"
 local Command
 local Command_Arguments={}
+local Seperator=" "
 
 function ADMIN(CallType, player)
     script.parent.Admin_Handler:SendMessage(CallType, player)
+end
+
+function splitString(inputstr)
+	local t = {}
+	for str in string.gmatch(inputstr, "([^"..Seperator.."]+)") do
+	    table.insert(t, str)
+	end
+	return t
+end
+
+function slice(tbl, first, last, step)
+  local sliced = {}
+
+  for i = first or 1, last or #tbl, step or 1 do
+    sliced[#sliced+1] = tbl[i]
+  end
+
+  return sliced
 end
 
 Chat.Received:connect(function(player, input)
@@ -17,7 +31,20 @@ Chat.Received:connect(function(player, input)
         local x,y=string.find(input, Prefix)
         if x==1 then
             -- TODO: Split arguments and command, etc
+        if Prefix ~= "" then 
+            if msg:sub(1,1) ~= Prefix then
+                return 
+            end
+            msg = msg:sub(2) --cut out the prefix
         end
+        msg = msg:lower()
+        --print("Got command "..msg) <- debug
+        local split = splitString(msg)
+        if #split < 2 then return end
+        local Command = split[1]
+        local Command_Arguments = {}
+        --print("command is "..command) 
+        Command_Arguments = slice(split, 2)
+        --TODO: what to do with the command and args?
     end
 end)
-
